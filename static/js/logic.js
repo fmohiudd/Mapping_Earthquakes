3a527a9d345b4c3c43a45009207593878a147f6c
fmohiudd <fmohiudd@uncc.edu>
2021-09-25 13:23:40 -0400

    Adding files and creating a new branch Mapping_GeoJSON_Polygons

--- static/js/logic.js
+++ static/js/logic.js
@@ -17,7 +17,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 });
 
 // We create the DARK tile layer that will be the background of our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -25,67 +25,45 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{
 
 // Create a base layer that holds both maps. 
 let baseMaps = {
-    Street: streets,
-    Dark: dark
+    "Streets": streets,
+    "SatelliteStreets": satelliteStreets
 };
 // // Then we add our 'streets' tile layer to the map.
 // streets.addTo(map);
 
 //  Method 2
-// Create the map object with a center, zoom level and default layer.
+// Create the map object with a center, zoom level and default layer to Toronto.
 let map = L.map("mapid", {
-    center: [30, 30],
-    zoom: 2,
+    center: [43.7, -79.3],
+    zoom: 11,
     layers: [streets]
 });
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/fmohiudd/Mapping_Earthquakes/main/majorAirports.json";
+let torontoHoods = "https://raw.githubusercontent.com/fmohiudd/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
-// Grabbing our GeoJASON data.
-d3.json(airportData).then(function(data) {
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffaaff", 
+    weight: 1
+};
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
     L.geoJSON(data, {
+        // sytle: myStyle,
+        // color: "ff4422",
+        // width: 1,
         onEachFeature:function(feature, layer) {
-            console.log(layer);
             layer
-            .bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2> <hr> <h3>Airport Name: " + feature.properties.name + "</h3>");
-        }
+            .bindPopup("<h2>Neighborhood: " + feature.properties.AREA_NAME + "</h2>");
+        },
+        sytle: myStyle
     }).addTo(map);
 });
 
 
-// // Grabbing our GeoJASON data.
-// d3.json(airportData).then(function(data) {
-//     console.log(data);
-//     // Creating a GeoJSON layer with the retrieved data.
-//     L.geoJSON(data).addTo(map);
-// });
-
-
-
-// To add multiple markers:the lat and log are nested in an array
-// We need to iterate through the array and add each lat & long.
-
-//Get data from cities.js
-// let cityData = cities;
-
-
-// // Loop through the cities array and create one marker for each city.
-// cityData.forEach(function(city) {
-//     console.log(city)
-//     // L.marker(city.location)
-//     // .bindPopup("<h2>" + city.city + ", " + city.state +  "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-//     // .addTo(map)
-
-//     // Add a circle marker with city population as the circle size.
-//     L.circleMarker(city.location, {
-//         radius: city.population/100000
-//     })
-//     .bindPopup("<h2>" + city.city + ", " + city.state +  "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-//     .addTo(map)
-// });
 
