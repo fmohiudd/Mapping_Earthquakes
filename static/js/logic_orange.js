3ab2b9523e27da2e794c56f448e5cae8b0bfcc07
fmohiudd <fmohiudd@uncc.edu>
2021-09-24 22:42:48 -0400

    Adding Mapping_Multiple_Points data

--- static/js/logic_orange.js
+++ static/js/logic_orange.js
@@ -5,18 +5,11 @@ console.log("working");
 // Method 1
 let map = L.map('mapid').setView([34.0522, -118.2437], 4);
 
-// // Method 2
-// let map = L.map('mapid', {
-//     center: [
-//         40.7, -94.5
-//     ],
-//     zoom: 4
-// });
 
 // We create the tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     // id: 'mapbox/streets-v11',
@@ -26,9 +19,6 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     // zoomOffset: -1,
     accessToken: API_KEY
 });
-
-// // Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 // To add multiple markers:the lat and log are nested in an array
 // We need to iterate through the array and add each lat & long.
@@ -40,13 +30,12 @@ let cityData = cities;
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
     console.log(city)
-    // L.marker(city.location)
-    // .bindPopup("<h2>" + city.city + ", " + city.state +  "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    // .addTo(map)
 
     // Add a circle marker with city population as the circle size.
     L.circleMarker(city.location, {
-        radius: city.population/100000
+        radius: city.population/200000,
+        color: "orange",
+        linewidth: "8"
     })
     .bindPopup("<h2>" + city.city + ", " + city.state +  "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
     .addTo(map)
