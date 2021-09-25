ac88ac61757e2e86fbde911075bc7a32376401f9
fmohiudd <fmohiudd@uncc.edu>
2021-09-25 13:07:51 -0400

    Uploading GeoJSON_Points file to a new branch

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,36 +3,12 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 // Method 1
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
-
-// Coordinates for each point to be used in the line. 
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-];
-
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "yellow"
-}).addTo(map);
-
-// // Method 2
-// let map = L.map('mapid', {
-//     center: [
-//         40.7, -94.5
-//     ],
-//     zoom: 4
-// });
+// let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    // id: 'mapbox/streets-v11',
     // other mapbox id: mapbox/outdoors-v11, mapbox/light-v10, mapbox/dark-v10, 
     // mapbox/satellite-v9, mapbox/satellite-streets-v11
     // tileSize: 512,
@@ -40,31 +16,76 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
     accessToken: API_KEY
 });
 
-// // Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// We create the DARK tile layer that will be the background of our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps. 
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+};
+// // Then we add our 'streets' tile layer to the map.
+// streets.addTo(map);
+
+//  Method 2
+// Create the map object with a center, zoom level and default layer.
+let map = L.map("mapid", {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+});
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/fmohiudd/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJASON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data, {
+        onEachFeature:function(feature, layer) {
+            console.log(layer);
+            layer
+            .bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2> <hr> <h3>Airport Name: " + feature.properties.name + "</h3>");
+        }
+    }).addTo(map);
+});
+
+
+// // Grabbing our GeoJASON data.
+// d3.json(airportData).then(function(data) {
+//     console.log(data);
+//     // Creating a GeoJSON layer with the retrieved data.
+//     L.geoJSON(data).addTo(map);
+// });
+
+
 
 // To add multiple markers:the lat and log are nested in an array
 // We need to iterate through the array and add each lat & long.
 
 //Get data from cities.js
-let cityData = cities;
+// let cityData = cities;
 
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    // L.marker(city.location)
-    // .bindPopup("<h2>" + city.city + ", " + city.state +  "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    // .addTo(map)
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     // L.marker(city.location)
+//     // .bindPopup("<h2>" + city.city + ", " + city.state +  "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     // .addTo(map)
 
-    // Add a circle marker with city population as the circle size.
-    L.circleMarker(city.location, {
-        radius: city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state +  "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map)
-});
+//     // Add a circle marker with city population as the circle size.
+//     L.circleMarker(city.location, {
+//         radius: city.population/100000
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state +  "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map)
+// });
 
-// Then we add our 'graymap' tile layer to the map.
-// Just checking
-streets.addTo(map);
